98136e8b33aa294bfd0ae93af139219501798292
David <davidphilipsumner@gmail.com>
2021-04-24 18:17:20 +0100

    Create GULP file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,43 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass')
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename')
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require("gulp-autoprefixer");
 
-
-
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+function styles() {
+  return gulp.src('scss/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer('last 2 versions'))
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(rename('styles.css'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('.'))
 }
 
-exports.default = defaultTask
+function server() {
+  console.log("\r\n Starting BrowserSync \r\n" );
+
+  browserSync.init({
+    server: {
+      baseDir: './',
+      index: 'index.html'
+    },
+  });
+};
+
+function reload(done) {
+  browserSync.reload();
+  done();
+};
+
+function watchFiles(done) {
+  gulp.watch('*.html', reload);
+  gulp.watch('./scss/*.scss', gulp.series(styles,reload));
+  gulp.watch('./js/*.js', reload)
+};
+
+// Spin up server & watch files
+exports.default = gulp.parallel(server, watchFiles);
