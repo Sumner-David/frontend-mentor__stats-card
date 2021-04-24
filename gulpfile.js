15a77bde1754fe463b5cf0b3e621992390b4cb72
David <davidphilipsumner@gmail.com>
2021-04-24 17:27:45 +0100

    base file structure

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass')
+
+
+
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
+exports.default = defaultTask
