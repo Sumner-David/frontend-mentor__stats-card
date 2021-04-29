61f2a40cf4ccebf15e83e5722502aaa9bf069321
David <davidphilipsumner@gmail.com>
2021-04-29 21:04:27 +0100

    Complete the design. Add fonts & use theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./js/**/*.js', './index.html'],
   darkMode: false,
@@ -12,9 +14,13 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: {
-          DEFAULT: '#C9A168',
-        }
+        'primary' : '#aa5cdb',
+        'secondary': '#1b1938',
+        'dark-blue': '#090b1a',
+      },
+      fontFamily: {
+        'sans': ['Inter', ...defaultTheme.fontFamily.serif],
+        'secondary': ['Lexend Deca', ...defaultTheme.fontFamily.sans]
       }
     }
   },
